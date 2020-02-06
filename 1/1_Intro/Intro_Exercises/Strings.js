f857ffe7400a9e22a92a02ae9fa83406e21bf5e9
Gheorghita <amaliacaldare20@gmail.com>
2020-02-06 10:16:01 +0100

    add new exercises

---
+++ 1/1_Intro/Intro_Exercises/Strings.js
@@ -0,0 +1,15 @@
+//Exercise 3
+var numberOne = "1.10";
+var numberTwo = "2.30";
+
+var sum = Number(numberOne) + Number(numberTwo);
+console.log(sum);
+
+//Exercise 4
+
+var one = 10;
+var two = 45;
+var three = 98;
+
+var avg = (+(one) + +(two) + + (three) ) / 3;
+console.log(avg);
