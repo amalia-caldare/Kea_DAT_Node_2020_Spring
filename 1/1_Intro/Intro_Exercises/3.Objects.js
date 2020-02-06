69cdf56ec3fd023494e93a4c4e38e28465325dc9
Gheorghita <amaliacaldare20@gmail.com>
2020-02-06 11:24:05 +0100

    object exercisses

--- 1/1_Intro/Intro_Exercises/3.Objects.js
+++ 1/1_Intro/Intro_Exercises/3.Objects.js
@@ -1,1 +1,24 @@
-var myObejct = {};
+//Exercise 1 - Retrieve value from object by key
+
+var myObj = {"message": "Hello, earthling! I bring peace."};
+console.log(myObj.message); //dot notation
+//2nd method
+console.log(myObj["message"]) //bracket notation
+ 
+//Exercise 2 - Defining an object
+
+var person = { "name": "Amalia",
+                "age": "20",
+
+};
+
+//Exercise 3 - Add a property
+
+var stackOverflow = {};
+stackOverflow.isAllowed = true;
+
+//Exercise 4 - Remove a property 
+
+var thisSong = {"description": "The best song in the world."}
+delete thisSong.description;
+thisSong["about"] = "Just a tribute";
