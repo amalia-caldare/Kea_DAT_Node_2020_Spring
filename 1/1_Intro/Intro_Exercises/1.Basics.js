ad0084332c494a7193e5eef0c9f203a523129de8
Gheorghita <amaliacaldare20@gmail.com>
2020-02-06 09:51:50 +0100

    added first exercises

---
+++ 1/1_Intro/Intro_Exercises/1.Basics.js
@@ -0,0 +1,17 @@
+//Exercise 1
+var firstName = "Anders";
+var lastName = "Latif";
+
+console.log("My name is",firstName,"and my lastName is" ,lastName);
+
+//Exercise 2
+//1st method
+var year = "2017";
+var number = 1;
+
+console.log(parseInt(year) + number);
+
+//2nd method
+var newYear = Number(year) + number; // +(year) + 1;
+console.log(newYear);
+
