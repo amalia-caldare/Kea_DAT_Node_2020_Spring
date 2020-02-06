0d0eace00d1a409e06ebba65432f2cae197e82a4
Gheorghita <amaliacaldare20@gmail.com>
2020-02-06 10:48:56 +0100

    new exercises soltuions

--- 1/1_Intro/Intro_Exercises/Strings.js
+++ 1/1_Intro/Intro_Exercises/Strings.js
@@ -1,15 +1,31 @@
-//Exercise 3
+//Exercise 3 - Add numbers from string to float
 var numberOne = "1.10";
 var numberTwo = "2.30";
 
 var sum = Number(numberOne) + Number(numberTwo);
 console.log(sum);
 
-//Exercise 4
+//Exercise 4 - Decimals and average
 
 var one = 10;
 var two = 45;
 var three = 98;
 
-var avg = (+(one) + +(two) + + (three) ) / 3;
+var avg = ((one+two + three) / 3).toFixed(5);
 console.log(avg);
+
+//Exercise 5 - Add the numbers and the total with 2 decimals
+var numberOne = "1.10";
+var numberTwo = "2.30";
+
+var result = ((parseFloat(numberOne) + parseFloat(numberTwo)).toFixed(2));
+console.log(result);
+
+//Exercise 6 - Get characters by index
+var letters = "abc";
+console.log(letters.charAt(2));
+
+//Exercise 7 - Replace
+var fact = "You are learning javascript";
+console.log(fact.replace("javascript","Javascript"));
+console.log(fact.indexOf("j"));
