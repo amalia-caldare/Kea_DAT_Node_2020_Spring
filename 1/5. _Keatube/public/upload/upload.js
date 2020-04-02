c77aa46d08c0efcb15c389646969ab371c26f67f
Gheorghita <amaliacaldare20@gmail.com>
2020-04-02 10:01:42 +0200

    Client-side validation of upload form

--- 1/5. _Keatube/public/upload/upload.js
+++ 1/5. _Keatube/public/upload/upload.js
@@ -1,3 +1,37 @@
+let fileValid = false;
 function validateForm() {
-    return true;
+    const title = document.forms.videoupload.title.value;
+    const description = document.forms.videoupload.description.value;
+    const tags = document.forms.videoupload.tags;
+    const category = document.forms.videoupload.category;
+    if(title.length < 8 || title.length > 64) {
+        //handle messages tot the user what went wrong;
+        return false;
+    }
+
+    if( description > 2048) {
+        return false;
+    }
+
+    return true && fileValid;
+}
+
+function handlefileUpload(files){
+    const file = files[0];
+
+    const mimeTypeArray = file.type("/");
+    if(mimeTypeArray[0] != "video") {
+    fileValid = false;
+    return;
+}
+    const fileSize = file.size;
+
+    const twoGbFileLimit = 2147483648;
+
+    if(fileSize > twoGbFileLimit) {
+        fileValid = false;
+        return;
+    }
+
+    fileValid = true;
 }
